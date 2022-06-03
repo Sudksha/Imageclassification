b331c43bb42de99ea741e6afda1ba13ff2474b04
Sudiksha Patil <74994512+Sudksha@users.noreply.github.com>
2022-06-04 01:15:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import flask
+import util
+
+artifacts=util.load_saved_artifacts()
+
+app=Flask(__name__, )
+
+@app.route('/')
+def home():
+    return flask.render_template('index.html')
+    
+@app.route('/classify_image', methods=['GET','POST'])
+def classify_image():
+    image_data= request.form['image_data']
+    print(artifacts,image_data)
+    print(util.classify_image(image_data,artifacts))
+    response = jsonify(util.classify_image(image_data,artifacts))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    
+    
+    
+    
+if __name__== "__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    
+    app.run(debug=True, port=5000)
